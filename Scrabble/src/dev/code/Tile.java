c257c8e5e2a410d86d1888ffad13f3ce020bbb50
Michael D <mdeiter@asu.edu>
2015-02-16 13:23:24 -0700

    Added to array to hold number of tiles per letter

--- Scrabble/src/dev/code/Tile.java
+++ Scrabble/src/dev/code/Tile.java
@@ -5,11 +5,20 @@ public class Tile {
 	int value = 0;
 	char letter = '\0';
 	static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	static String letterValue = "1332142418513113X11114484X";
+	static String letterValue = "1332142418513113X11114484X"; /* X = 10 */
+	static String numOfLetter = "9224Z232911426821646422121"; /* Z = 12 */
 
-	static String[][] letterAry = new String[26][2];
+	static String[][] letterAry = new String[26][3];
 
 	/*
+	 * Tile distribution A: 9 tiles B: 2 tiles C: 2 tiles D: 4 tiles E: 12 tiles
+	 * F: 2 tiles G: 3 tiles H: 2 tiles I: 9 tiles J: 1 tile K: 1 tile L: 4
+	 * tiles M: 2 tiles N: 6 tiles O: 8 tiles P: 2 tiles Q: 1 tile R: 6 tiles S:
+	 * 4 tiles T: 6 tiles U: 4 tiles V: 2 tiles W: 2 tiles X: 1 tile Y: 2 tiles
+	 * Z: 1 tile
+	 */
+
+	/* Tile value:
 	 * A is worth 1 B is worth 3 C is worth 3 D is worth 2 E is worth 1 F is
 	 * worth 4 G is worth 2 H is worth 4 I is worth 1 J is worth 8 K is worth 5
 	 * L is worth 1 M is worth 3 N is worth 1 O is worth 1 P is worth 3 Q is
@@ -57,9 +66,15 @@ public class Tile {
 		for (int i = 0; i < 26; i++) {
 			letterAry[i][0] = String.valueOf(alphabet.charAt(i));
 			letterAry[i][1] = String.valueOf(letterValue.charAt(i));
+			letterAry[i][2] = String.valueOf(numOfLetter.charAt(i));
 
+			// Convert X to 10 if found in letterValue array
 			if (letterAry[i][1].equals("X")) {
 				letterAry[i][1] = "10";
+			}
+			// Convert Z to 12 if found in numOfLetter array
+			if (letterAry[i][2].equals("Z")) {
+				letterAry[i][2] = "12";
 			}
 		}
 	}
